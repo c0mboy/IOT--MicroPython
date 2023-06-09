4fbedd692a8e71f916d04af832f603cd2eb227fa
c0mboy <90767788+c0mboy@users.noreply.github.com>
2023-06-09 13:08:19 +0200

    Took away the led color thingy didnt w

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import machine
 # Create a Pin object for controlling the LED
 pin = Pin("LED", Pin.OUT)
 
+
 # Create a DHT11 object for temperature and humidity measurement
 tempSensor = dht.DHT11(machine.Pin(27))    # DHT11 sensor connected to GPIO 27 
 
@@ -19,12 +20,7 @@ while True:
     # Toggle the LED state (on or off)
     pin.toggle()
 
-    # Print temperature and humidity
-    print("LED is ON changing color")
-    pwm = PWM(pin)
-    pwm.freq(1000)
-    pwm.duty_u16(0)
-    
+
     print("Temperature is {} degrees Celsius and Humidity is {}%".format(temperature, humidity))
 
     # Pause for 5 seconds
