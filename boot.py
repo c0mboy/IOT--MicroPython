bfb75638c909371015d16787b431c20dc3483068
c0mboy <90767788+c0mboy@users.noreply.github.com>
2023-06-12 10:44:55 +0200

    connect code wifi

--- boot.py
+++ boot.py
@@ -1,1 +1,17 @@
 # boot.py -- run on boot-up
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)       # Put modem on Station mode
+    if not sta_if.isconnected():                # Check if already connected
+        print('connecting to network...')
+        sta_if.active(True)                     # Activate network interface
+        sta_if.connect('La Familia', 'kaminen25')     # Your WiFi Credential
+        # Check if it is connected otherwise wait
+        while not sta_if.isconnected():         
+            pass
+    # Print the IP assigned by router
+    print('network config:', sta_if.ifconfig()) 
+
+# WiFi Connection
+do_connect()
--- main.py
+++ main.py
@@ -4,6 +4,27 @@ from machine import Pin
 import dht
 import machine
 
+
+def http_get(url = 'http://detectportal.firefox.com/'):
+    import socket                           # Used by HTML get request
+    import time                             # Used for delay
+    _, _, host, path = url.split('/', 3)    # Separate URL request
+    addr = socket.getaddrinfo(host, 80)[0][-1]  # Get IP address of host
+    s = socket.socket()                     # Initialise the socket
+    s.connect(addr)                         # Try connecting to host address
+    # Send HTTP request to the host with specific path
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))    
+    time.sleep(1)                           # Sleep for a second
+    rec_bytes = s.recv(10000)               # Receve response
+    print(rec_bytes)                        # Print the response
+    s.close()                               # Close connection
+
+
+# HTTP request
+http_get()
+
+
+
 # Create a Pin object for controlling the LED
 pin = Pin("LED", Pin.OUT)
 
@@ -31,3 +52,4 @@ while True:
         # If the temperature reading dosn't give u values then it will give an error
     except OSError:
         print("Failed to read sensor.")       
+
